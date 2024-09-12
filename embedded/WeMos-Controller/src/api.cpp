c21d5ed84f173e45aee75851fa1738a910297862
Melvin Moes <melvin.moes@hva.nl>
2024-09-12 15:32:55 +0200

    Able to fetch data from embedded device

--- embedded/WeMos-Controller/src/api.cpp
+++ embedded/WeMos-Controller/src/api.cpp
@@ -44,33 +44,18 @@ void Api::CreatePlayer(String name)
     HTTPClient http;
 
     http.begin(wifiClient, "http://moesmq.loca.lt/api/player");
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
     // String httpRequestData = "name=" + name;           
     // int httpResponseCode = http.POST(httpRequestData);
     int httpResponseCode = http.GET();
     Serial.println("HTTP Response code: " + String(httpResponseCode));
+    
+    if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println("Response: " + response);
+    } else {
+        Serial.println("Error on sending POST request");
+    }
 
-    // if(IsConnected()) {
-    //     Serial.println("HELP ME PLS");
-    // }
-    // WiFiClient wifiClient;
-    // HTTPClient http;
-
-    // // Specify the correct endpoint, assuming it's /player for creating a player
-    // http.begin(wifiClient, "http://TUNNEL-URL/api/player");
-
-    // // Add header to specify we're sending form-encoded data
-    // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    // // Format the data for the POST body
-    // // String httpRequestData = "name=" + name;
-
-    // // Send POST request
-    // int httpResponseCode = http.GET();
-
-    // // Print the response code
-    // Serial.println("HTTP Response code: " + String(httpResponseCode));
-
-    // // End the HTTP connection
-    // http.end();
+    http.end();
 }
