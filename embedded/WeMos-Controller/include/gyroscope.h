91cc052889ec92739dfcb17d779a3d8d81d37764
Melvin Moes <melvin.moes@hva.nl>
2024-09-16 13:03:48 +0200

    Working DMP MPU code

--- embedded/WeMos-Controller/include/gyroscope.h
+++ embedded/WeMos-Controller/include/gyroscope.h
@@ -2,7 +2,14 @@
 #define GYROSCOPE_H
 
 #include "utilities.h"
-#include "MPU6050.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
 
 class Gyroscope {
     public:
@@ -12,26 +19,55 @@ class Gyroscope {
         void Loop();
         void CalibrateGyro();
     private:
-        //MPU6050 accelgyro(0x69); // <-- use for AD0 high
-        //MPU6050 accelgyro(0x68, &Wire1); // <-- use for AD0 low, but 2nd Wire (TWI/I2C) object
+        MPU6050 mpu;
+        // uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+        // quaternion components in a [w, x, y, z] format (not best for parsing
+        // on a remote host such as Processing or something though)
+        //#define OUTPUT_READABLE_QUATERNION
 
-        int16_t ax, ay, az;
-        int16_t gx, gy, gz;
+        // uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+        // (in degrees) calculated from the quaternions coming from the FIFO.
+        // Note that Euler angles suffer from gimbal lock (for more info, see
+        // http://en.wikipedia.org/wiki/Gimbal_lock)
+        //#define OUTPUT_READABLE_EULER
 
-        // uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
-        // list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
-        // not so easy to parse, and slow(er) over UART.
-        #define OUTPUT_READABLE_ACCELGYRO
+        // uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+        // pitch/roll angles (in degrees) calculated from the quaternions coming
+        // from the FIFO. Note this also requires gravity vector calculations.
+        // Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+        // more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+        #define OUTPUT_READABLE_YAWPITCHROLL
 
-        // uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
-        // binary, one right after the other. This is very fast (as fast as possible
-        // without compression or data loss), and easy to parse, but impossible to read
-        // for a human.
-        //#define OUTPUT_BINARY_ACCELGYRO
+        // uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
+        // components with gravity removed. This acceleration reference frame is
+        // not compensated for orientation, so +X is always +X according to the
+        // sensor, just without the effects of gravity. If you want acceleration
+        // compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
+        // #define OUTPUT_READABLE_REALACCEL
 
+        // uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
+        // components with gravity removed and adjusted for the world frame of
+        // reference (yaw is relative to initial orientation, since no magnetometer
+        // is present in this case). Could be quite handy in some cases.
+        //#define OUTPUT_READABLE_WORLDACCEL
 
-        #define LED_PIN LED_BUILTIN
-        bool blinkState = false;
+        #define INTERRUPT_PIN 15  // use pin 2 on Arduino Uno & most boards
+
+        // MPU control/status vars
+        bool dmpReady = false;  // set true if DMP init was successful
+        uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+        uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+        uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+        uint16_t fifoCount;     // count of all bytes currently in FIFO
+        uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+        // orientation/motion vars
+        Quaternion q;           // [w, x, y, z]         quaternion container
+        VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+        VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+        VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+        VectorFloat gravity;    // [x, y, z]            gravity vector
+        float euler[3];         // [psi, theta, phi]    Euler angle container
+        float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 };
-
 #endif
--- embedded/WeMos-Controller/src/gyroscope.cpp
+++ embedded/WeMos-Controller/src/gyroscope.cpp
@@ -1,129 +1,156 @@
 #include "gyroscope.h"
-#include "Wire.h"
 
-MPU6050 accelgyro;
+Gyroscope::Gyroscope() {}
+Gyroscope::~Gyroscope(){}
 
-Gyroscope::Gyroscope() 
-{
-
-}
-
-Gyroscope::~Gyroscope() 
-{
-
+// INTERRUPT DETECTION ROUTINE
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void IRAM_ATTR dmpDataReady() {
+    mpuInterrupt = true;
 }
 
 void Gyroscope::Init() 
 {
-    Serial.println("----[Initializing Gyroscope]----");
-    Wire.begin();
-    accelgyro.initialize();
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
 
     // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-    // use the code below to change accel/gyro offset values
-    Serial.println("Updating internal sensor offsets...");
-    Serial.println( 
-        String(accelgyro.getXAccelOffset()) + "\t" +        // -76
-        String(accelgyro.getYAccelOffset()) + "\t" +        // -2359
-        String(accelgyro.getZAccelOffset()) + "\t" +        // 1688
-        String(accelgyro.getXGyroOffset()) + "\t" +         // 0
-        String(accelgyro.getYGyroOffset()) + "\t" +         // 0
-        String(accelgyro.getZGyroOffset())                  // 0
-    );              
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
 
-    CalibrateGyro();
-    accelgyro.setXGyroOffset(91.00000);
-    accelgyro.setYGyroOffset(-5.00000);
-    accelgyro.setZGyroOffset(10.00000);
-    accelgyro.setZAccelOffset(1574.00000);
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
 
-    Serial.println( 
-        String(accelgyro.getXAccelOffset()) + "\t" +        // -76
-        String(accelgyro.getYAccelOffset()) + "\t" +        // -2359
-        String(accelgyro.getZAccelOffset()) + "\t" +        // 1688
-        String(accelgyro.getXGyroOffset()) + "\t" +         // 0
-        String(accelgyro.getYGyroOffset()) + "\t" +         // 0
-        String(accelgyro.getZGyroOffset())                  // 0
-    );           
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(87.00000);
+    mpu.setYGyroOffset(51.00000);
+    mpu.setZGyroOffset(10.00000);
+    mpu.setZAccelOffset(1558.00000); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
 }
 
-struct MyData {
-  byte X;
-  byte Y;
-  byte Z;
-};
-
-MyData data;
 void Gyroscope::Loop()
 {
-    // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    // data.X = map(gx, -17000, 17000, 0, 255 ); // X axis data
-    // data.Y = map(gy, -17000, 17000, 0, 255); 
-    // data.Z = map(gz, -17000, 17000, 0, 255);  // Y axis data
     delay(500);
-    
-    // these methods (and a few others) are also available
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+        #ifdef OUTPUT_READABLE_QUATERNION
+            // display quaternion values in easy matrix form: w x y z
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            Serial.print("quat\t");
+            Serial.print(q.w);
+            Serial.print("\t");
+            Serial.print(q.x);
+            Serial.print("\t");
+            Serial.print(q.y);
+            Serial.print("\t");
+            Serial.println(q.z);
+        #endif
 
-    #ifdef OUTPUT_READABLE_ACCELGYRO
-        // display tab-separated accel/gyro x/y/z values
-        Serial.print("a/g:\t");
-        // Serial.print(ax); Serial.print("\t");
-        // Serial.print(ay); Serial.print("\t");
-        // Serial.print(az); Serial.print("\t");
-        Serial.print(gx); Serial.print("\t");
-        Serial.print(gy); Serial.print("\t");
-        Serial.println(gz);
-        // Serial.print("Axis X = ");
-        // Serial.print(data.X);
-        // Serial.print("  ");
-        // Serial.print("Axis Y = ");
-        // Serial.print(data.Y);
-        // Serial.print("  ");
-        // Serial.print("Axis Z  = ");
-        // Serial.println(data.Z);
-    #endif
+        #ifdef OUTPUT_READABLE_EULER
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
+            Serial.print("euler\t");
+            Serial.print(euler[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(euler[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(euler[2] * 180/M_PI);
+        #endif
 
-    #ifdef OUTPUT_BINARY_ACCELGYRO
-        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
-        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
-        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
-        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
-        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
-        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
-    #endif
-}
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+        #endif
 
-void Gyroscope::CalibrateGyro()
-{
-    accelgyro.CalibrateAccel(6);
-    accelgyro.CalibrateGyro(6);
-    Serial.println("\nat 600 Readings");
-    accelgyro.PrintActiveOffsets();
-    Serial.println();
-    accelgyro.CalibrateAccel(1);
-    accelgyro.CalibrateGyro(1);
-    Serial.println("700 Total Readings");
-    accelgyro.PrintActiveOffsets();
-    Serial.println();
-    accelgyro.CalibrateAccel(1);
-    accelgyro.CalibrateGyro(1);
-    Serial.println("800 Total Readings");
-    accelgyro.PrintActiveOffsets();
-    Serial.println();
-    accelgyro.CalibrateAccel(1);
-    accelgyro.CalibrateGyro(1);
-    Serial.println("900 Total Readings");
-    accelgyro.PrintActiveOffsets();
-    Serial.println();    
-    accelgyro.CalibrateAccel(1);
-    accelgyro.CalibrateGyro(1);
-    Serial.println("1000 Total Readings");
-    accelgyro.PrintActiveOffsets();
-    Serial.println("\n\n Any of the above offsets will work nice \n\n Lets proof the PID tuning using another method:"); 
+        #ifdef OUTPUT_READABLE_REALACCEL
+            // display real acceleration, adjusted to remove gravity
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            Serial.print("areal\t");
+            Serial.print(aaReal.x);
+            Serial.print("\t");
+            Serial.print(aaReal.y);
+            Serial.print("\t");
+            Serial.println(aaReal.z);
+        #endif
+
+        #ifdef OUTPUT_READABLE_WORLDACCEL
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            Serial.print("aworld\t");
+            Serial.print(aaWorld.x);
+            Serial.print("\t");
+            Serial.print(aaWorld.y);
+            Serial.print("\t");
+            Serial.println(aaWorld.z);
+        #endif
+    }
 }
