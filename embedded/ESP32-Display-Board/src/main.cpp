c196a726d7e2df5b0c9a222518f75914e0de499b
Melvin Moes <melvin.moes@hva.nl>
2024-09-26 13:34:58 +0200

    Added basic sprite render

--- embedded/ESP32-Display-Board/src/main.cpp
+++ embedded/ESP32-Display-Board/src/main.cpp
@@ -1,28 +1,21 @@
-#include <Arduino.h>
-/*
-  An example showing rainbow colours on a 3.5" TFT LCD screen
-  and to show a basic example of font use.
-
-  Make sure all the display driver and pin connections are correct by
-  editing the User_Setup.h file in the TFT_eSPI library folder.
-#include "BluetoothServer.h"
-
-  Note that yield() or delay(0) must be called in long duration for/while
-  loops to stop the ESP8266 watchdog triggering.
-
-  #########################################################################
-  ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
-  #########################################################################
-*/
-
+#include "utilities.h"
 #include <TFT_eSPI.h>
+#include "BluetoothServer.h"
+#include "sprites/sprites.h"
+
+
 BluetoothServer bluetooth;
 
+/**
+ * DONT FORGET TO CHANGE THE USER_SETUP.H IN THE TFT_eSPI lib inside .pio/libdeps
+ */
 TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite cursorSprite= TFT_eSprite(&tft);
 
 void setup() {
   Serial.begin(115200);
   bluetooth.Init();
+
   // Start the tft display and set it to black
   tft.init();
   tft.setRotation(1); //This is the display in landscape
@@ -30,20 +23,34 @@ void setup() {
   // Clear the screen before writing to it
   tft.fillScreen(TFT_BLACK);
 
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  int x = 5;
-  int y = 10;
-  int fontNum = 2; 
-  tft.drawString("Hello", x, y, fontNum); // Left Aligned
-  x = 320 /2;
-  y += 16;
-  tft.setTextColor(TFT_BLUE, TFT_BLACK);
-  tft.drawCentreString("World", x, y, fontNum);
+  // tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  // int x = 5;
+  // int y = 10;
+  // int fontNum = 2; 
+  // tft.drawString("Hello", x, y, fontNum); // Left Aligned
+  // x = 320 /2;
+  // y += 16;
+  // tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  // tft.drawCentreString("World", x, y, fontNum);
 
+
+
+  cursorSprite.createSprite(16,16);
+  cursorSprite.setSwapBytes(true);
+  cursorSprite.pushImage(0, 0, 16, 16, cursor); // Use pushImage if cursor is in PROGMEM
+  cursorSprite.pushSprite(50, 50, TFT_BLACK);
 }
-
+float x = 10;
 void loop() {
-  // put your main code here, to run repeatedly:
   bluetooth.Loop();
+  // tft.drawString("X: " + String(bluetooth.gyroData.x) + " Y: " + String(bluetooth.gyroData.y), 100, 100, 2); // Left Aligned
+  // tft.fillScreen(TFT_BLACK);
+  //cursorSprite.pushImage(0,0,16,16, &cursor);
+  cursorSprite.pushSprite(x, 50);
+  
+  x++;
 
+  if(x>330)
+  x=-100;
+  delay(100);
 }
