56a616bf8bcec74e7557b1d70bdd072ddd874299
Melvin Moes <melvin.moes@hva.nl>
2024-10-04 10:34:38 +0200

    Restyled admin page

---
+++ web/js/admin.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('/api/player')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                //Create columns
+                const id = document.createElement('td');
+                id.className = 'p-2';
+                id.textContent = element.id;
+                
+                const name = document.createElement('td');
+                name.className = 'p-2';
+                name.textContent = element.name;
+                
+                // Create a row
+                const row = document.createElement('tr');
+                row.className = 'hover:bg-gray-50 border-t-2 border-gray-100';
+                row.appendChild(id);
+                row.appendChild(name);
+                
+                // Append id and name to table body
+                document.getElementById("players").appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
