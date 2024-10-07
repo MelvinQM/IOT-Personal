24324f3e3163e7f9a387831b131615595bb281f0
Melvin Moes <melvin.moes@hva.nl>
2024-10-07 15:27:52 +0200

    Added hover effect to tables

--- web/js/admin.js
+++ web/js/admin.js
@@ -5,16 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
             data.forEach(element => {
                 //Create columns
                 const id = document.createElement('td');
-                id.className = 'p-2';
                 id.textContent = element.id;
                 
                 const name = document.createElement('td');
-                name.className = 'p-2';
                 name.textContent = element.name;
                 
                 // Create a row
                 const row = document.createElement('tr');
-                row.className = 'hover:bg-gray-50 border-t-2 border-gray-100';
                 row.appendChild(id);
                 row.appendChild(name);
                 
@@ -34,20 +31,16 @@ document.addEventListener('DOMContentLoaded', function () {
             data.forEach(element => {
                 //Create columns
                 const id = document.createElement('td');
-                id.className = 'p-2';
                 id.textContent = element.id;
                 
                 const sessionId = document.createElement('td');
-                sessionId.className = 'p-2';
                 sessionId.textContent = element.session_id;
                 
                 const score = document.createElement('td');
-                score.className = 'p-2';
                 score.textContent = element.score;
                 
                 // Create a row
                 const row = document.createElement('tr');
-                row.className = 'hover:bg-gray-50 border-t-2 border-gray-100';
                 row.appendChild(id);
                 row.appendChild(sessionId);
                 row.appendChild(score);
@@ -68,32 +61,25 @@ document.addEventListener('DOMContentLoaded', function () {
             data.forEach(element => {
                 //Create columns
                 const id = document.createElement('td');
-                id.className = 'p-2';
                 id.textContent = element.id;
                 
                 const completed = document.createElement('td');
-                completed.className = 'p-2';
                 completed.textContent = element.completed ? "True" : "False";
                 
                 const startTime = document.createElement('td');
-                startTime.className = 'p-2';
                 startTime.textContent = element.start_time;
 
                 const endTime = document.createElement('td');
-                endTime.className = 'p-2';
                 endTime.textContent = element.end_time;
                 
                 const playerId = document.createElement('td');
-                playerId.className = 'p-2';
                 playerId.textContent = element.player_id;
                 
                 const difficultyId = document.createElement('td');
-                difficultyId.className = 'p-2';
                 difficultyId.textContent = element.difficulty_id;
 
                 // Create a row
                 const row = document.createElement('tr');
-                row.className = 'hover:bg-gray-50 border-t-2 border-gray-100';
                 row.appendChild(id);
                 row.appendChild(completed);
                 row.appendChild(startTime);
@@ -115,17 +101,13 @@ document.addEventListener('DOMContentLoaded', function () {
             data.forEach(element => {
                 //Create columns
                 const id = document.createElement('td');
-                id.className = 'p-2';
                 id.textContent = element.id;
                 
                 const name = document.createElement('td');
-                name.className = 'p-2';
                 name.textContent = element.name;
                 
-
                 // Create a row
                 const row = document.createElement('tr');
-                row.className = 'hover:bg-gray-50 border-t-2 border-gray-100';
                 row.appendChild(id);
                 row.appendChild(name);
                 
@@ -136,4 +118,5 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => {
             console.error('Error:', error);
         });
+        
 });
