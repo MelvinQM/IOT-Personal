463b2d629950dc8db2d80a7fd8475e53c5658502
Melvin Moes <melvin.moes@hva.nl>
2024-10-07 15:20:29 +0200

    Added a difficulties table to the admin page

--- web/js/admin.js
+++ web/js/admin.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-        fetch('/api/session')
+    fetch('/api/session')
         .then(response => response.json())
         .then(data => {
             data.forEach(element => {
@@ -103,6 +103,34 @@ document.addEventListener('DOMContentLoaded', function () {
                 
                 // Append id and name to table body
                 document.getElementById("sessions").appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    fetch('/api/difficulty')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                //Create columns
+                const id = document.createElement('td');
+                id.className = 'p-2';
+                id.textContent = element.id;
+                
+                const name = document.createElement('td');
+                name.className = 'p-2';
+                name.textContent = element.name;
+                
+
+                // Create a row
+                const row = document.createElement('tr');
+                row.className = 'hover:bg-gray-50 border-t-2 border-gray-100';
+                row.appendChild(id);
+                row.appendChild(name);
+                
+                // Append id and name to table body
+                document.getElementById("difficulties").appendChild(row);
             });
         })
         .catch(error => {
