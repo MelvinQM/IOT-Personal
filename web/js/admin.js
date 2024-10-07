a51e9b462436ac05857b74776b66cb33e7bbd9d4
Melvin Moes <melvin.moes@hva.nl>
2024-10-07 13:21:48 +0200

    Showing score on admin page

--- web/js/admin.js
+++ web/js/admin.js
@@ -25,4 +25,40 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => {
             console.error('Error:', error);
         });
+
+
+
+    fetch('/api/score')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+
+                console.log(element)
+                //Create columns
+                const id = document.createElement('td');
+                id.className = 'p-2';
+                id.textContent = element.id;
+                
+                const sessionId = document.createElement('td');
+                sessionId.className = 'p-2';
+                sessionId.textContent = element.session_id;
+                
+                const score = document.createElement('td');
+                score.className = 'p-2';
+                score.textContent = element.score;
+                
+                // Create a row
+                const row = document.createElement('tr');
+                row.className = 'hover:bg-gray-50 border-t-2 border-gray-100';
+                row.appendChild(id);
+                row.appendChild(sessionId);
+                row.appendChild(score);
+                
+                // Append id and name to table body
+                document.getElementById("scores").appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
